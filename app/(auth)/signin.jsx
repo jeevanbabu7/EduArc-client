f524c5319a662f80703364cb8b54925001604c78
joeljose111 <joeljosetj101@gmai.com>
2025-03-14 15:56:35 +0530

    --

--- app/(auth)/signin.jsx
+++ app/(auth)/signin.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, SafeAreaView, KeyboardAvoidingView, Platform, Alert, Pressable} from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, SafeAreaView, KeyboardAvoidingView, Platform, Alert, Pressable } from 'react-native';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import {auth} from '../../firebase.js';
 import { useRouter } from 'expo-router';
