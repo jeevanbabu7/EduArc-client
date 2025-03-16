f61d5751355447192765499840a716fa80a5badb
jeevanbabu7 <jeevanbabu2710@gmail.com>
2025-03-16 21:11:52 +0530

    cohere model for chat bot

--- app/(chat)/Chatbot.jsx
+++ app/(chat)/Chatbot.jsx
@@ -12,7 +12,8 @@ import getEnvVars from '../../config.js';
 import { useToast } from '@gluestack-ui/themed';
 import { ID } from 'react-native-appwrite';
 import { storage } from '../../lib/appwrite/appwrite.js';
-
+import axios from 'axios';
+import { streamModelResponse } from '../../lib/cohere.js';
 
 const ChatBot = () => {
   console.log(IP_ADDRESS);
@@ -146,8 +147,8 @@ const ChatBot = () => {
         }
       } catch (error) {
         console.error(error);
-      }
     }
+  }
 
     fetchPreviousMessages();
   }, []);
@@ -183,28 +184,28 @@ const ChatBot = () => {
   
   const handleSend = async () => {
     try {
-      if(fileURL){
-        // save the file to the chroma database 
-        const response = await fetch('http://127.0.0.1:5000/api/upload', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            file_link: `https://cloud.appwrite.io/v1/storage/buckets/67bccd990005a5d175c4/files/${fileID}/view?project=67bcccfe0010a29974a4&mode=admin`
-          })
-        });
-        const data = await response.json();
-        console.log(data);
-        const userMessage = {
-          _id: Math.random().toString(36).substring(7),
-          text: fileURL,
-          createdAt: new Date(),
-          user: {
-            _id: 1,
-          },
-        };
-      }
+      // if(fileURL){
+      //   // save the file to the chroma database 
+      //   const response = await fetch('http://127.0.0.1:5000/api/upload', {
+      //     method: 'POST',
+      //     headers: {
+      //       'Content-Type': 'application/json'
+      //     },
+      //     body: JSON.stringify({
+      //       file_link: `https://cloud.appwrite.io/v1/storage/buckets/67bccd990005a5d175c4/files/${fileID}/view?project=67bcccfe0010a29974a4&mode=admin`
+      //     })
+      //   });
+      //   const data = await response.json();
+      //   console.log(data);
+      //   const userMessage = {
+      //     _id: Math.random().toString(36).substring(7),
+      //     text: fileURL,
+      //     createdAt: new Date(),
+      //     user: {
+      //       _id: 1,
+      //     },
+      //   };
+      // }
       
       if (inputText.trim()) {
         const userMessage = {
@@ -216,17 +217,35 @@ const ChatBot = () => {
           },
         };
         
-        socket.emit('send-message', {
-            chatSessionId: "67d3ef1f6d1812088b501946",
-            sender: "user",
-            content: inputText
-        });
+        // socket.emit('send-message', {
+        //     chatSessionId: "67d3ef1f6d1812088b501946",
+        //     sender: "user",
+        //     content: inputText
+        // });
   
   
         
         setMessages((previousMessages) => GiftedChat.append(previousMessages, [userMessage]));
         // onSend([userMessage]);
         setInputText('');
+
+        await streamModelResponse(inputText, (token) => {
+          // setResponse((prev) => prev + token); // Append token to response
+          console.log(token); // Print each token to the console
+        });
+        
+  
+        // const botMessage = {
+        //   _id: Math.random().toString(36).substring(7),
+        //   text: botmsg,
+        //   createdAt: new Date(),
+        //   user: {
+        //     _id: 2,
+        //     name: 'ChatBot',
+        //     avatar: 'https://placekitten.com/100/100',
+        //   },
+        // };
+        // setMessages((previousMessages) => GiftedChat.append(previousMessages, [botMessage]));
       }
     }catch(error) {
       console.error(error);
